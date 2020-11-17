ba137747aa285a01e1a6e55b7d2a0441ed4f3cf9
toby495 <64807700+toby495@users.noreply.github.com>
2020-11-17 15:11:14 -0600

    update_categorias

--- front-tpv/src/components/NavBar.jsx
+++ front-tpv/src/components/NavBar.jsx
@@ -2,44 +2,114 @@ import React from 'react';
 import '../navbar-styles/navbar.css';
 import '../navbar-styles/navbar-mobile.css';
 import '../navbar-styles/navbar-desktop.css';
+
 export default class NavBar extends React.Component{
 
     constructor(props){
         super(props);
         this.state ={
-            section:[]
+            inve:{
+                borde:''
+            },
+            sucu:{
+                borde:''
+            },
+            emple:{
+                borde:''
+            },
+            vent:{
+                borde:''
+            }
         }
     }
-    componentDidMount(){
-       this.setState({
-           section:[{
-               id:'1',
-               name:'Inventario'
-           },
-           {
-               id:'2',
-               name:'Empleados'
-           },
-           {
-               id:'3',
-               name:'Sucursales'
-           },
-           {
-               id:'4',
-               name:'Ventas'
-           }
-        ]
-       })
-    }
+
+   handleInve = (e) =>{
+        this.setState({
+            inve:{
+                borde:'dashed',
+                component:'Login'
+            },
+            sucu:{
+                borde:''
+            },
+            emple:{
+                borde:''
+            },
+            vent:{
+                borde:''
+            },
+        })
+   }
+
+   handleSucu = (e) =>{
+    this.setState({
+        inve:{
+            borde:''
+        },
+        sucu:{
+            borde:'dashed'
+        },
+        emple:{
+            borde:''
+        },
+        vent:{
+            borde:''
+        }
+    })
+}
+
+handleEmpl = (e) =>{
+    this.setState({
+        inve:{
+            borde:''
+        },
+        sucu:{
+            borde:''
+        },
+        emple:{
+            borde:'dashed'
+        },
+        vent:{
+            borde:''
+        }
+    })
+}
+
+handleVent = (e) =>{
+    this.setState({
+        inve:{
+            borde:''
+        },
+        sucu:{
+            borde:''
+        },
+        emple:{
+            borde:''
+        },
+        vent:{
+            borde:'dashed'
+        }
+    })
+}
+    
+    
     render(){
         return(
             <div className="main-nav">
                <ul>
-                   {this.state.section.map((type)=>{
-                       return <li key={type.id}>
-                           <button id="type">{type.name}</button>
-                       </li>
-                   })}
+                   <li>
+                       <button onClick={this.handleInve} style={{border:this.state.inve.borde}} className="type">Inventario</button>
+                   </li>
+                   <li>
+                       <button onClick={this.handleSucu} style={{border:this.state.sucu.borde}} className="type">Sucursales</button>
+                   </li>
+                   <li>
+                       <button onClick={this.handleEmpl} style={{border:this.state.emple.borde}} className="type">Empleados</button>
+                   </li>
+                   <li>
+                       <button onClick={this.handleVent} style={{border:this.state.vent.borde}} className="type">Ventas</button>
+                   </li>
+                   
                </ul>
             </div>
         );
